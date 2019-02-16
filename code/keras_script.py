038bc6ae026aec2b44de710715c606c7840c3a74
Diego Rueda <ing.diegorueda@gmail.com>
2019-02-15 23:10:04 -0500

    minor error in floats fixed

--- code/keras_script.py
+++ code/keras_script.py
@@ -152,16 +152,16 @@ def get_callback_list(network, path, models_dir, logs_dir):
         TensorBoard(log_dir=f'{logs_dir}/{network}/{path}')]
     return callback_list
 
-def focal_loss(gamma=2, alpha=4):
+def focal_loss(gamma=2.0, alpha=4.0):
 
     def focal_loss_fixed(y_true, y_pred):
-        epsilon = 1.e-9
+        epsilon = 1e-9
         y_true = tf.convert_to_tensor(y_true, tf.float32)
         y_pred = tf.convert_to_tensor(y_pred, tf.float32)
 
         model_out = tf.add(y_pred, epsilon)
         ce = tf.multiply(y_true, -tf.log(model_out))
-        weight = tf.multiply(y_true, tf.pow(tf.subtract(1, model_out), gamma))
+        weight = tf.multiply(y_true, tf.pow(tf.subtract(1.0, model_out), gamma))
         f1 = tf.multiply(alpha, tf.multiply(weight, ce))
         reduced_f1 = tf.reduce_max(f1, axis=1)
         return tf.reduce_mean(reduced_f1)
