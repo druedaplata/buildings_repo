88e3f44b50a10e9fcb96e9029cfe06ffafa30060
Diego Rueda <ing.diegorueda@gmail.com>
2019-04-03 17:54:55 -0500

    custom generator for multimodal data using augmentation and standardize on images

--- code/keras_script.py
+++ code/keras_script.py
@@ -112,30 +112,17 @@ def get_image_generator(images_dir, split, *args):
 def get_combined_generator(images_dir, csv_dir, csv_data, split, *args):
 
     img_width, img_height, batch_size = args
-    # Create image preprocessing
-    # TODO: Change this, it doesn't work on apply preprocessing
-    data_gen_args = dict(
-        horizontal_flip=True,
-        brightness_range=[0.5, 1.5],
-        shear_range=10,
-        zoom_range=0.2,
-        rotation_range=15,
-        channel_shift_range=50,
-        rescale=1. / 255
-    )
-
-    datagen = ImageDataGenerator()
-
-    generator = datagen.flow_from_directory(
-        f'{images_dir}/{split}',
-        target_size=(img_width, img_height),
-        batch_size=batch_size,
-        shuffle=True,
-        class_mode='categorical')
 
     image_file_list = glob(f'{images_dir}/{split}/**/*.JPG', recursive=True)
-    df = pd.read_csv(f'{csv_dir}/{split}.csv', index_col=csv_data[0])
+    dirs = sorted(os.listdir(f'{images_dir}/{split}'))
+    num_classes = len(dirs)
+    num_images = len(image_file_list)
+    classes = {v: i for i, v in enumerate(dirs)}
     random.shuffle(image_file_list)
+
+    df = pd.read_csv(f'{csv_dir}/{split}.csv', index_col=csv_data[0])
+
+    datagen = ImageDataGenerator()
 
     def my_generator(images_list, dataframe, batch_size):
         i = 0
@@ -145,30 +132,35 @@ def get_combined_generator(images_dir, csv_dir, csv_data, split, *args):
                 if i == len(images_list):
                     i = 0
                     random.shuffle(images_list)
+
+                # Load image
                 image_path = images_list[i]
                 # Remove last .JPG from image, generated images have two.
                 image_name = os.path.basename(image_path)[:-4]
-                image = datagen.apply_transform(
-                    krs_image.load_img(
-                        image_path,
-                        target_size=(img_height, img_width),
-                    ),
-                    data_gen_args,
-                )
+                image = krs_image.load_img(
+                    image_path, target_size=(img_height, img_width))
                 image = krs_image.img_to_array(image)
 
                 i += 1
-
+                # Get label from CSV
                 csv_attrs = dataframe.loc[image_name, :]
-                label = generator.class_indices[csv_attrs['clase']]
+                label = classes[csv_attrs['clase']]
                 csv_attrs = csv_attrs.drop(labels='clase')
+
                 batch['images'].append(image)
                 batch['csv'].append(csv_attrs)
                 batch['labels'].append(label)
 
-            batch['images'] = np.array(batch['images'])
+            # Convert images to batch form
+            batch['images'] = np.array(batch['images'], dtype=np.uint8)
+            # Standardize images in batch
+            batch['images'] = datagen.standardize(batch['images'])
+            # Apply preprocessing to image batch
+            batch['images'] = preprocessing.augment_images(batch['images'])
+
             batch['csv'] = np.array(batch['csv'])
-            batch['labels'] = np.eye(generator.num_classes)[batch['labels']]
+            batch['labels'] = np.eye(len(dirs))[batch['labels']]
+
             yield [batch['images'], batch['csv']], batch['labels']
 
     csv_generator = my_generator(image_file_list, df, batch_size)
@@ -177,7 +169,7 @@ def get_combined_generator(images_dir, csv_dir, csv_data, split, *args):
     # Minus the index column
     features -= 1
 
-    return csv_generator, generator, features
+    return num_images, num_classes, features, csv_generator
 
 
 def get_cnn_model(network, input_shape, main_input, *args):
@@ -398,24 +390,11 @@ def train_combined(network, images_dir, csv_dir, csv_data, *args):
     img_width, img_height, batch_size, lr_rate, epochs, models_dir, logs_dir, gpu_number = args
 
     # Get combined and image generators, and number of features in csv files.
-    multi_train_gen, train_gen, features = get_combined_generator(
-        images_dir,
-        csv_dir,
-        csv_data,
-        'train',
-        img_width,
-        img_height,
-        batch_size,
-    )
+    num_images_train, num_classes_train, features, multi_train_gen = get_combined_generator(
+        images_dir, csv_dir, csv_data, 'train', img_width, img_height, batch_size)
 
-    multi_val_gen, val_gen, _ = get_combined_generator(
-        images_dir,
-        csv_dir,
-        csv_data,
-        'val',
-        img_width,
-        img_height,
-        batch_size,
+    num_images_val, num_classes_val, features, multi_val_gen = get_combined_generator(
+        images_dir, csv_dir, csv_data, 'val', img_width, img_height, batch_size
     )
 
     # Get network model for an image input shape
@@ -428,14 +407,16 @@ def train_combined(network, images_dir, csv_dir, csv_data, *args):
     )
 
     # Make sure train/val have the same number of classes
-    num_classes = len(np.unique(train_gen.classes))
-    assert num_classes == len(np.unique(val_gen.classes))
+    assert num_classes_train == num_classes_val
 
     # Create class weights, useful for imbalanced datasets
-    class_weights = class_weight.compute_class_weight(
-        'balanced', np.unique(train_gen.classes), train_gen.classes)
+    # class_weights = class_weight.compute_class_weight(
+    #    'balanced', np.unique(train_gen.classes), train_gen.classes)
 
-    #class_weights = {0:48, 1:1, 2:1, 3:27, 4:30, 5:39, 6:12, 7:1}
+    if num_classes_train == 8:
+        class_weights = {0: 50, 1: 1, 2: 1, 3: 50, 4: 50, 5: 50, 6: 50, 7: 1}
+    if num_classes_train == 7:
+        class_weights = {0: 50, 1: 1, 2: 1, 3: 50, 4: 50, 5: 50, 6: 1}
 
     # Get network model and change last layers for training
     x = base_model.output
@@ -452,7 +433,7 @@ def train_combined(network, images_dir, csv_dir, csv_data, *args):
     # Merge both networks
     # TODO: Test with different number of layers after merge.
     merge = concatenate([x, aux])
-    predictions = Dense(num_classes, activation='softmax')(merge)
+    predictions = Dense(num_classes_train, activation='softmax')(merge)
 
     # Create model object in keras for both types of inputs
     model = Model(inputs=[main_input, aux_input], outputs=predictions)
@@ -484,10 +465,10 @@ def train_combined(network, images_dir, csv_dir, csv_data, *args):
     # Train the model on train split, for half the epochs
     model.fit_generator(
         multi_train_gen,
-        steps_per_epoch=train_gen.n // batch_size,
+        steps_per_epoch=num_images_train // batch_size,
         epochs=epochs // 2,
         validation_data=multi_val_gen,
-        validation_steps=val_gen.n // batch_size,
+        validation_steps=num_images_val // batch_size,
         class_weight=class_weights,
         callbacks=callback_list,
         use_multiprocessing=True)
@@ -513,10 +494,10 @@ def train_combined(network, images_dir, csv_dir, csv_data, *args):
     # Train the model on train split, for the second half epochs
     model.fit_generator(
         multi_train_gen,
-        steps_per_epoch=train_gen.n // batch_size,
+        steps_per_epoch=num_images_train // batch_size,
         epochs=epochs // 2,
         validation_data=multi_val_gen,
-        validation_steps=val_gen.n // batch_size,
+        validation_steps=num_images_val // batch_size,
         class_weight=class_weights,
         callbacks=callback_list,
         use_multiprocessing=True)
@@ -557,5 +538,5 @@ if __name__ == '__main__':
             logs_dir, gpu_number
         ]
 
-        train_on_images(network, images_dir, *args)
-        #train_combined(network, images_dir, csv_dir, csv_data, *args)
+        #train_on_images(network, images_dir, *args)
+        train_combined(network, images_dir, csv_dir, csv_data, *args)
