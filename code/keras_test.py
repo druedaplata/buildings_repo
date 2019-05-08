dd71d7a6a9c8d0879c871347e206c62be1b8c25e
Diego Rueda <ing.diegorueda@gmail.com>
2019-05-08 10:09:23 -0500

    change cm report to include csv values

--- code/keras_test.py
+++ code/keras_test.py
@@ -6,7 +6,10 @@ from glob import glob
 import configparser
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, fbeta_score
+import tensorflow as tf
+from tqdm import tqdm
 
+from keras.models import load_model
 from keras import metrics
 from keras.optimizers import Adam
 from keras import backend as K, Model
@@ -18,47 +21,74 @@ from keras.applications import InceptionV3, ResNet50, VGG16, VGG19, Xception
 from keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard, ReduceLROnPlateau
 
 from keras_script import get_image_generator, get_combined_generator, get_cnn_model
+import keras_metrics as km
 
 
-def get_matrix_from_gen(main_gen, model, split, name, net_id, output_dir, aux_gen=None):
-    print('Creating matrix...')
-    if aux_gen:
-        print(f'aux_gen: {aux_gen.n}')
-        y_pred = model.predict_generator(
-            main_gen, steps=len(aux_gen), use_multiprocessing=False)
-    else:
-        aux_gen = main_gen
-        y_pred = model.predict_generator(main_gen, use_multiprocessing=False, steps=len(aux_gen), verbose=1)
+def get_report(generator, num_images, num_classes, batch_size, model, split, net_name, net_id, figures_dir):
+    # Get predictions from model
+    print(f'Predictions from {split}')
+
+    y_true = np.array([])
+    y_pred = np.array([])
+
+    for step, (img_batch, batch_true) in tqdm(enumerate(generator)):
+        if step >= num_images//batch_size:
+            break
+        batch_pred = model.predict_on_batch(img_batch)
+        if step == 0:
+            y_pred = batch_pred
+            y_true = batch_true
+        else:
+            y_pred = np.vstack((y_pred, batch_pred))
+            y_true = np.vstack((y_true, batch_true))
+
+    # y_pred = model.predict_generator(
+    #    generator, steps=num_images/batch_size, use_multiprocessing=False, verbose=1)
+
     y_pred = np.argmax(y_pred, axis=-1)
-    y_true = aux_gen.classes
+    y_true = np.argmax(y_true, axis=-1)
 
-    print(y_pred.shape)
-    print(y_true.shape)
+    output_csv = pd.DataFrame({
+        'true': y_true,
+        'pred': y_pred})
+    output_csv.to_csv(
+        f'{figures_dir}/{net_id}{net_name}_{split}.csv', index=False)
 
-    f2_score = fbeta_score(y_true, y_pred, beta=2, average='weighted')
-    classes = list(aux_gen.class_indices.keys())
+    f2_score_beta = fbeta_score(y_true, y_pred, beta=2, average='micro')
+    #classes = list(aux_gen.class_indices.keys())
+
+    if num_classes == 7:
+        classes = ['CR_LDUAL', 'CR_LINF_DNO',
+                   'CR_LINF_DUC', 'CR_LWAL', 'MCF', 'MR', 'MUR']
+    if num_classes == 8:
+        classes = ['CR_LDUAL', 'CR_LINF_DNO', 'CR_LINF_DUC',
+                   'CR_LWAL', 'MCF', 'MCF_DNO', 'MR', 'MUR']
 
     cm = confusion_matrix(y_true, y_pred)
     cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
     plt.figure(figsize=(10, 10))
-    sns.heatmap(cm, annot=True, fmt='.2f',
-                xticklabels=classes, yticklabels=classes)
+    sns.heatmap(
+        cm, annot=True, fmt='.2f', xticklabels=classes, yticklabels=classes)
     plt.yticks(rotation=0)
-    plt.title(f'{net_id}{name}_{split} -> f2={f2_score:.3}')
-    plt.savefig(f'{output_dir}/{net_id}{name}_{split}.png')
+    plt.title(f'{net_id}{net_name}_{split} -> f2={f2_score_beta:.3}')
+    plt.savefig(f'{figures_dir}/{net_id}{net_name}_{split}.png')
 
 
 def test_on_images(network, images_dir, models_dir, *args):
     """"""
     print('Testing on images...')
     # Extract parameters from args
-    img_width, img_height, batch_size, lr_rate, epochs, models_dir, logs_dir, figures_dir = args
+    img_width, img_height, batch_size, models_dir, figures_dir = args
 
     # Get image generators
-    val_gen = get_image_generator(
+    num_images_val, num_classes_val, val_gen = get_image_generator(
         images_dir, 'val', img_width, img_height, batch_size)
-    test_gen = get_image_generator(
+
+    num_images_test, num_classes_test, test_gen = get_image_generator(
         images_dir, 'test', img_width, img_height, batch_size)
+
+    # Make sure val/test have the same number of classes
+    assert num_classes_val == num_classes_test
 
     # Get network model for an image input shape
     input_shape = (img_width, img_height, 3)
@@ -66,40 +96,48 @@ def test_on_images(network, images_dir, models_dir, *args):
     base_model, last_layer_number = get_cnn_model(
         network, input_shape, main_input)
 
-    # Make sure val/test have the same number of classes
-    num_classes = len(np.unique(val_gen.classes))
-    assert num_classes == len(np.unique(val_gen.classes))
-
     x = base_model.output
     x = GlobalAveragePooling2D()(x)
-    x = Dense(1024, activation='relu')(x)
-    predictions = Dense(num_classes, activation='softmax')(x)
+    predictions = Dense(num_classes_val, activation='softmax')(x)
     model = Model(base_model.input, outputs=predictions)
 
     path = f'{models_dir}/{network}'
-    models_list = glob(f'{path}/A*',)
+    models_list = glob(f'{path}/A*', )
 
     net_id = f'{os.path.basename(models_dir)}A_'
     print('Loading model...')
     for h5_model in models_list:
-        model.load_weights(h5_model)
+        if gpu_number > 1:
+            multi_gpu_model = load_model(
+                h5_model,
+                custom_objects={
+                    'categorical_f1_score': km.categorical_f1_score()
+                })
+            model = multi_gpu_model.layers[-2]
+        else:
+            model.load_weights(h5_model)
         print('Validation')
-        get_matrix_from_gen(val_gen, model, 'val',
-                            network, net_id, figures_dir)
+        get_report(val_gen, num_images_val, num_classes_val, batch_size,
+                   model, 'val', network, net_id, figures_dir)
         print('Test')
-        get_matrix_from_gen(test_gen, model, 'test',
-                            network, net_id, figures_dir)
+        get_report(test_gen, num_images_test, num_classes_test, batch_size,
+                   model, 'test', network, net_id, figures_dir)
 
 
 def test_combined(network, images_dir, csv_dir, csv_data, models_dir, *args):
     # Extract parameters from args
-    img_width, img_height, batch_size, lr_rate, epochs, models_dir, logs_dir, figures_dir = args
+    img_width, img_height, batch_size, models_dir, figures_dir = args
 
     # Get image generators
-    multi_val_gen, val_gen, features = get_combined_generator(
+
+    num_images_val, num_classes_val, features, val_gen = get_combined_generator(
         images_dir, csv_dir, csv_data, 'val', img_width, img_height, batch_size)
-    multi_test_gen, test_gen, _ = get_combined_generator(
+
+    num_images_test, num_classes_test, features, test_gen = get_combined_generator(
         images_dir, csv_dir, csv_data, 'test', img_width, img_height, batch_size)
+
+    # Make sure val/test have the same number of classes
+    assert num_classes_val == num_classes_test
 
     # Get network model for an image input shape
     input_shape = (img_width, img_height, 3)
@@ -107,40 +145,42 @@ def test_combined(network, images_dir, csv_dir, csv_data, models_dir, *args):
     base_model, last_layer_number = get_cnn_model(
         network, input_shape, main_input)
 
-    # Make sure val/test have the same number of classes
-    num_classes = len(np.unique(val_gen.classes))
-    assert num_classes == len(np.unique(val_gen.classes))
-
     x = base_model.output
     x = GlobalAveragePooling2D()(x)
-    x = Dense(1024, activation='relu')(x)
 
     # Load Simple MLP
-    aux_input = Input(shape=(features,))
-    aux = Dense(4096, activation='relu')(aux_input)
+    aux_input = Input(shape=(features, ))
+    aux = Dense(256, activation='relu')(aux_input)
     aux = Dropout(0.3)(aux)
-    aux = Dense(4096, activation='relu')(aux)
+    aux = Dense(128, activation='relu')(aux)
     aux = Dropout(0.3)(aux)
-    aux = Dense(1024, activation='relu')(aux)
+    aux = Dense(64, activation='relu')(aux)
 
     # Merge input models
     merge = concatenate([x, aux])
-    #merge = Dense(1024, activation='relu')(merge)
-    #merge = Dropout(0.3)(merge)
-    #merge = Dense(1024, activation='relu')(merge)
-    predictions = Dense(8, activation='softmax')(merge)
+    predictions = Dense(num_classes_val, activation='softmax')(merge)
     model = Model(inputs=[main_input, aux_input], outputs=predictions)
 
     path = f'{models_dir}/{network}'
-    models_list = glob(f'{path}/B*',)
+    models_list = glob(f'{path}/B*', )
     net_id = f'{os.path.basename(models_dir)}B_'
 
     for h5_model in models_list:
-        model.load_weights(h5_model)
-        get_matrix_from_gen(multi_val_gen, model, 'val',
-                            network, net_id, figures_dir, val_gen)
-        get_matrix_from_gen(multi_test_gen, model, 'test',
-                            network, net_id, figures_dir, test_gen)
+        if gpu_number > 1:
+            multi_gpu_model = load_model(
+                h5_model,
+                custom_objects={
+                    'categorical_f1_score': km.categorical_f1_score()
+                },
+            )
+            model = multi_gpu_model.layers[-2]
+        else:
+            model.load_weights(h5_model)
+
+        get_report(val_gen, num_images_val, num_classes_val,
+                   batch_size, model, 'val', network, net_id, figures_dir)
+        get_report(test_gen, num_images_val, num_classes_val,
+                   batch_size, model, 'test', network, net_id, figures_dir)
 
 
 if __name__ == '__main__':
@@ -162,7 +202,7 @@ if __name__ == '__main__':
     networks_list = config.get('TRAINING', 'cnn_network_list').split(',')
     gpu_number = config.getint('TRAINING', 'gpu_number')
 
-    batch_size = batch_size * gpu_number
+    # batch_size = batch_size * gpu_number
 
     # Read data paths
     models_dir = config.get('OUTPUT', 'models_dir')
@@ -172,8 +212,7 @@ if __name__ == '__main__':
 
     for network in networks_list:
 
-        args = [img_width, img_height, batch_size,
-                lr_rate, epochs, models_dir, logs_dir, figures_dir]
+        args = [img_width, img_height, batch_size, models_dir, figures_dir]
 
         test_on_images(network, images_dir, models_dir, *args)
         test_combined(network, images_dir, csv_dir,
