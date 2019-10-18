3e7c8d9529a0ee69e54b0a0a0f8fb4cc87d7b011
Diego Rueda <ing.diegorueda@gmail.com>
2019-10-18 15:39:35 -0500

    include config and run.sh from apolo

--- code/run.sh
+++ code/run.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-#SBATCH --partition=accel
+#SBATCH --partition=accel-2
 #SBATCH --nodes=1                                                             
-#SBATCH --ntasks-per-node=32
+#SBATCH --ntasks-per-node=8
 #SBATCH --gres=gpu:1
 #SBATCH --time=14-00:00:00
-#SBATCH --job-name=edificios
-#SBATCH -o macro.out      # File to which STDOUT will be written
-#SBATCH -e macro.err      # File to which STDERR will be written
+#SBATCH --job-name=12d8
+#SBATCH -o output/12d8.out      # File to which STDOUT will be written
+#SBATCH -e output/12d8.err      # File to which STDERR will be written
 ##SBATCH --mail-type=ALL
 ##SBATCH --mail-user=ing.diegorueda@gmail.com
 
 export SBATCH_EXPORT=NONE
 export OMP_NUM_THREADS=1
 
-module load cuda/8.0
+module load cuda/9.0
 module load python/3.5.2_intel-2017_update-1
 
 source activate keras_intel
 
-python keras_script.py
+python train.py
